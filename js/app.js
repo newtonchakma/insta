4ac05b8436d635a735f5cda8ffa1d1e52f3e79e3
newtonchakma <newtonchakma304@gmail.com>
2022-03-11 23:31:56 +0600

    final

--- js/app.js
+++ js/app.js
@@ -40,10 +40,9 @@ const switchTab = (id) => {
         document.getElementById( "reported" ).style.display = "none";
     } else if (id === "liked") {
         document.getElementById( "liked" ).style.display = "block";
-        document.getElementById( "liked-title" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
-        document.getElementById( "reported-title" ).style.display = "none";
+     
 
         displayLikedPosts();
     } else {
